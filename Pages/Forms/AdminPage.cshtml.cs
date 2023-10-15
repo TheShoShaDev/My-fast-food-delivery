848c76cf1afae9cb6815d0188d9bcb0c885f7c0b
TheShoShaDev <ratattyu007@mail.ru>
2023-10-15 20:51:22 +0700

    Starting adding AdminPage logic

--- Pages/Forms/AdminPage.cshtml.cs
+++ Pages/Forms/AdminPage.cshtml.cs
@@ -1,3 +1,4 @@
+using Grill1.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -7,8 +8,11 @@ namespace Grill1.Pages.Forms
 	//[Authorize]
 	public class AdminPageModel : PageModel
     {
+        ProductModel Product = new ProductModel();
+
         public void OnGet()
         {
+            
         }
     }
 }
